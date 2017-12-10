9c0e030c6d325fadacbd72cab645f41f99138b12
Omar <o_sanchez999@hotmail.com>
2017-12-10 13:21:28 -0600

    Reparacion de icono

--- js/arriba.js
+++ js/arriba.js
@@ -1,9 +1,15 @@
-// JavaScript Document
 $(document).ready(function(){
+	
 	$('.ir-arriba').click(function(){
 		$('body, html').animate({
 			scrollTop: '0px'
 		}, 700);
+	});	
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
 	});			  
-				  
 });
